320f8ffbcec9c746bd3d9a2e309fea4cdefd4099
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2022-12-21 00:20:36 +0530

    add file based logging support

--- elastic-fluentbit-kibana/build-image.sh
+++ elastic-fluentbit-kibana/build-image.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Building go code..."
-# go build -ldflags '-s -w -extldflags "-static"' -o server .
-# CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-s -w -extldflags "-static"' -o server .
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-s -w -extldflags "-static"' -o server .
 
 echo -e "\nBuilding docker image..."
-# docker build -t server:latest . || exit 1
+docker build -t server:latest . || exit 1
 
 # echo -e "\nStarting docker container...\n"
 # docker run --rm -it server:latest
--- elastic-fluentbit-kibana/server.go
+++ elastic-fluentbit-kibana/server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"path/filepath"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 
@@ -10,48 +12,67 @@ import (
 	"os"
 )
 
-const port string = ":8080"
+const (
+	port           string = ":8080"
+	logFilePath    string = "logrus.log"
+	envFileLogging string = "FILE_LOGGING_ENABLED"
+)
 
 var hostname string
 var logger *log.Entry
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	username := req.URL.Query().Get("username")
-	if username != "" {
-		logger.Debugf("Found username in the query: %s", username)
-		logger = log.WithFields(log.Fields{
-			"username": username,
-		})
-	}
+	logger := logger
+	logger = log.WithFields(log.Fields{
+		"host":            req.Host,
+		"method":          req.Method,
+		"uri":             req.RequestURI,
+		"remoteAddr":      req.RemoteAddr,
+		"x-real-ip":       req.Header.Get("X-Real-IP"),
+		"x-forwarded-for": req.Header.Get("X-Forwarded-For"),
+		"x-host":          req.Header.Get("Host"),
+	})
 
 	n := rand.Intn(10)
 	if n < 4 {
 		logger.Errorf("Internal server error! n: %d", n)
-		fmt.Fprintf(w, "Internal server error! serverId: %s\n", hostname)
 		w.WriteHeader(500)
+		fmt.Fprintf(w, "Internal server error! serverId: %s\n", hostname)
 		return
 	}
 
 	if n > 3 && n < 7 {
 		logger.Warnf("Not found! n: %d", n)
-		fmt.Fprintf(w, "Not found! serverId: %s\n", hostname)
 		w.WriteHeader(404)
+		fmt.Fprintf(w, "Not found! serverId: %s\n", hostname)
 		return
 	}
-
-	fmt.Fprintf(w, "hello %s! serverId: %s\n", username, hostname)
-	logger.Infof("%s [%s] Remote Addr: %s", req.Method, req.RequestURI, req.RemoteAddr)
+	logger.Infof("hello from serverId: %s", hostname)
+	fmt.Fprintf(w, "hello from serverId: %s\n", hostname)
 }
 
 func main() {
 	hostname, _ = os.Hostname()
+	var enableFileLogging bool
+	v, ok := os.LookupEnv(envFileLogging)
+	if ok {
+		enableFileLogging, _ = strconv.ParseBool(v)
+	}
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
 	logger = log.WithFields(log.Fields{
 		"serverId": hostname,
 	})
-
+	if enableFileLogging {
+		logFilePathAbs, _ := filepath.Abs(logFilePath)
+		file, err := os.OpenFile(logFilePathAbs, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("Failed to enable file logging due to %s", err)
+		}
+		logger.Infof("File logging enabled, please tail %s for further logs", logFilePathAbs)
+		log.SetOutput(file)
+	}
 	http.HandleFunc("/", hello)
 	logger.Infof("Starting server on port %s", port)
 	http.ListenAndServe(port, nil)
